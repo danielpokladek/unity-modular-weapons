9de9570f6b9e957e7adac32d52b6fed45d7e046d
Daniel Pokladek <danielpokladek@proton.me>
2026-02-16 15:32:14 +0000

    adds a frame to attachment items

--- Assets/Project/Scritps/UI/ItemUI.cs
+++ Assets/Project/Scritps/UI/ItemUI.cs
@@ -6,5 +6,10 @@ public class ItemUI : MonoBehaviour
     [SerializeField]
     private Image _itemImage;
 
+    [SerializeField]
+    Button _button;
+
     public Image ItemImage => _itemImage;
+
+    public Button Button => _button;
 }
