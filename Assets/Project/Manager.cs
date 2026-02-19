af035443e8a26009cca946f7830a457bad634c0c
Daniel Pokladek <danielpokladek@proton.me>
2026-02-19 15:58:40 +0000

    camera will only auto zoom to attachment if enabled in settings

--- Assets/Project/Manager.cs
+++ Assets/Project/Manager.cs
@@ -1,6 +1,13 @@
 #nullable enable
 
+using System;
 using UnityEngine;
+
+[Serializable]
+public struct AppSettings
+{
+    public bool AutoZoomToAttachment;
+}
 
 public class Manager : MonoBehaviour
 {
@@ -20,17 +27,24 @@ public class Manager : MonoBehaviour
     [SerializeField]
     Weapon _currentWeapon = null!;
 
-    public static Manager Instance { get; private set; }
+    private AppSettings _settings;
 
     private AttachmentPoint? _currentAttachmentPoint;
 
     private void Awake()
     {
+        _settings = LoadSettings();
+        SaveSettings();
+
         Instance = this;
 
         Events.OnAttachmentPointFocus.AddListener((point) => _currentAttachmentPoint = point);
         Events.OnAttachmentPointUnfocus.AddListener(() => _currentAttachmentPoint = null);
     }
+
+    public static Manager Instance { get; private set; }
+
+    public AppSettings Settings => _settings;
 
     public Canvas Canvas => _uiCanvas;
     public UIController UIController => _uiController;
@@ -42,4 +56,19 @@ public class Manager : MonoBehaviour
     public AttachmentPoint? CurrentAttachmentPoint => _currentAttachmentPoint;
 
     public Weapon? CurrentWeapon => _currentWeapon;
+
+    private AppSettings LoadSettings()
+    {
+        var settings = new AppSettings
+        {
+            AutoZoomToAttachment = PlayerPrefs.GetInt("AutoZoomToAttachment") != 0,
+        };
+
+        return settings;
+    }
+
+    private void SaveSettings()
+    {
+        PlayerPrefs.SetInt("AutoZoomToAttachment", _settings.AutoZoomToAttachment ? 1 : 0);
+    }
 }
